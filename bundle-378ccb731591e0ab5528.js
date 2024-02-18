c9d35496b8c53e54bfaa43c6406476d9978aef7e
Sergey Koksharov <gentoosiastic@gmail.com>
2024-02-18 15:42:30 +0300

    Updates

--- bundle-378ccb731591e0ab5528.js
+++
@@ -1,1 +0,0 @@
-(()=>{(()=>{"use strict";var _={};console.log(`env vars: ${JSON.stringify("MISSING_ENV_VAR",null,2)}`)})();})();
---
+++ bundle-a5de264bef432e0e7d08.js
@@ -0,0 +1,1 @@
+(()=>{(()=>{"use strict";var _={};console.log("MY_API_URL=https://example.com"),console.log("MY_API_KEY=abracadabra")})();})();
